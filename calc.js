930a3d86d6efb4030c43367c1896276f24e020b4
Christiano Eduardo Dutra e Silva <christianoeds@gmail.com>
2026-08-13 10:46:17 -0300

    adicionando a feature para calculo de juros compostos

--- calc.js
+++ calc.js
@@ -3,3 +3,9 @@ function juros(c,i) {
     resultado =  (c * i) /1000;
     return resultado;
 }
+
+//calculo de juro composto
+function jurosCompostos(c, i, t) {
+    resultado = c * (1 + i)^t;
+    return resultado;
+}
